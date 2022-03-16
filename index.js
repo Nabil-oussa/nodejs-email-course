cec4d36db430db0e13b2d453e0c8c4d4c997f73c
nabil GHANMI <nabil.ghanmi@ausy.fr>
2022-03-16 11:55:15 +0100

    2eme commit

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Ici nous allons créer le serveur
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true, useUnifiedTopology: true});
+
+db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log("connecté à Mongoose")
+});
